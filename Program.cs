3dd5f08b48cc08bb15818d3e58c1c2b88d482150
DGubskyGUU <105551085+DGubskyGUU@users.noreply.github.com>
2025-02-26 21:48:20 +0300

    num2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,39 @@ namespace prac_25._2
     {
         static void Main(string[] args)
         {
+            // Создание объекта с использованием конструктора по умолчанию
+            Software software1 = new Software();
+            Console.WriteLine(software1.ToString());
+
+            // Создание объекта с использованием конструктора с параметрами
+            Software software2 = new Software("Windows", "Microsoft", true);
+            Console.WriteLine(software2.ToString());
+
+            // Ввод данных с консоли и создание объекта
+            Console.WriteLine("Введите название ПО:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите производителя:");
+            string manufacturer = Console.ReadLine();
+            Console.WriteLine("Платное ли ПО? (true/false):");
+            bool isPaid = bool.Parse(Console.ReadLine());
+
+            Software software3 = new Software(name, manufacturer, isPaid);
+            Console.WriteLine(software3.ToString());
+
+            // Демонстрация метода TogglePaymentStatus
+            Console.WriteLine("\nИзменение статуса оплаты для software3:");
+            software3.TogglePaymentStatus();
+            Console.WriteLine(software3.ToString());
+
+            // Демонстрация метода IsFreeFromManufacturer
+            Console.WriteLine("\nПроверка, является ли ПО бесплатным продуктом заданного производителя:");
+            Console.WriteLine("Введите производителя для проверки:");
+            string checkManufacturer = Console.ReadLine();
+            bool isFree = software3.IsFreeFromManufacturer(checkManufacturer);
+            Console.WriteLine($"ПО {software3.Name} от {checkManufacturer} бесплатное: {isFree}");
+
+            // Ожидание ввода пользователя перед закрытием консоли
+            Console.ReadLine();
         }
     }
 }
---
+++ Software.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_25._2
+{
+    internal class Software
+    {
+        // Поля
+        private string name;
+        private string manufacturer;
+        private bool isPaid;
+
+        // Свойства с методами доступа get и set
+        public string Name
+        {
+            get { return (name != "") ? name : "неизвестно"; }
+            set => name = value;
+        }
+
+        public string Manufacturer
+        {
+            get { return (manufacturer != "") ? manufacturer : "неизвестно"; }
+            set => manufacturer = value;
+        }
+
+        public bool IsPaid
+        {
+            get => isPaid;
+            set => isPaid = value;
+        }
+
+        // Конструктор по умолчанию
+        public Software()
+        {
+            Name = "неизвестно";
+            Manufacturer = "неизвестно";
+            IsPaid = false;
+        }
+
+        // Конструктор с параметрами
+        public Software(string name, string manufacturer, bool isPaid)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            IsPaid = isPaid;
+        }
+
+        // Метод для изменения поля платное/бесплатное на противоположное
+        public void TogglePaymentStatus()
+        {
+            IsPaid = !IsPaid;
+        }
+
+        // Метод для проверки, является ли ПО бесплатным продуктом заданного производителя
+        public bool IsFreeFromManufacturer(string manufacturer)
+        {
+            return Manufacturer == manufacturer && !IsPaid;
+        }
+
+        // Метод для вывода значений всех полей
+        public override string ToString()
+        {
+            return $"Название: {Name}, Производитель: {Manufacturer}, Платное: {(IsPaid ? "Да" : "Нет")}";
+        }
+    }
+}
+
